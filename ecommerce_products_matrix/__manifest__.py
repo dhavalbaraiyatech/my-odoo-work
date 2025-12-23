1b32930bd7a3dbc48c32b295d53e232245b785d9
Dhaval Baraiya <baraiyad@emiprotechnologies.com>
2025-12-23 09:50:28 +0530

    [ADD] Added a new customer module for display product in matrix format.

---
+++ ecommerce_products_matrix/__manifest__.py
@@ -0,0 +1,24 @@
+# __manifest__.py
+{
+    'name': 'eCommerce Products Matrix',
+    'version': '1.0.0',
+    'summary': '####',
+    'category': 'Website',
+    'author': 'Dhaval Baraiya.',
+    'website': 'https://dhavalbaraiya.odoo.com/',
+    'license': 'OPL-1',
+    'depends': ['website', 'website_sale', 'website_sale_stock'],
+    'data': [
+        'views/templates.xml',
+        'views/variant_templates.xml',
+        'views/product_views.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'ecommerce_products_matrix_ept/static/src/js/variant_cart_autoupdate.js',
+            'ecommerce_products_matrix_ept/static/src/js/variant_change.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+}
